9bf4d55eb7ed850e9161784e8ef55c3969f18a50
kortirso <kortirso@gmail.com>
2018-11-30 21:45:12 +0200

    save user_session_id in session

--- lib/langtool_web/controllers/page_controller.ex
+++ lib/langtool_web/controllers/page_controller.ex
@@ -2,6 +2,19 @@ defmodule LangtoolWeb.PageController do
   use LangtoolWeb, :controller
 
   def index(conn, _params) do
+    conn = check_user_session_id(conn)
+
     render conn, "index.html"
+  end
+
+  defp check_user_session_id(conn) do
+    message = conn |> get_session(:user_session_id)
+    if message == nil, do: conn |> put_session(:user_session_id, random_string(24)), else: conn
+  end
+
+  defp random_string(length) do
+    :crypto.strong_rand_bytes(length)
+    |> Base.url_encode64
+    |> binary_part(0, length)
   end
 end
