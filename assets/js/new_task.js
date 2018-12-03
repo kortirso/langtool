bb1f09af5e754a72d73cbd4d804c3e6ffcb1f383
kortirso <kortirso@gmail.com>
2018-12-03 20:32:33 +0200

    make http request from vue component

--- assets/js/new_task.js
+++ assets/js/new_task.js
@@ -1,3 +1,7 @@
+import VueResource from 'vue-resource'
+
+Vue.use(VueResource)
+
 new Vue({
   el: '#new_task',
   data: {
@@ -16,6 +20,16 @@ new Vue({
     uploadFile: function(event) {
       this.file = event.target.files[0]
       this.fileName = "Selected: " + event.target.files[0].name
+    },
+    createTask: function() {
+      let data = new FormData()
+      data.append('task[file]', this.file)
+      data.append('task[from]', this.from)
+      data.append('task[to]', this.to)
+      const config = { header : { 'Content-Type' : 'multipart/form-data' } }
+      this.$http.post('http://localhost:4000/tasks', data, config).then(function(data) {
+        console.log(data)
+      })
     }
   }
 })
