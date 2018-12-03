f70acdfb3cca52d3fc6eab30cb59f34002a3f679
kortirso <kortirso@gmail.com>
2018-12-03 15:51:34 +0200

    modify new_task form with vuejs actions

--- assets/js/new_task.js
+++ assets/js/new_task.js
@@ -1,6 +1,21 @@
 new Vue({
   el: '#new_task',
   data: {
-    message: 'Get started now'
+    extensions: {'ruby_on_rails': '.yml'},
+    extension: '',
+    framework: '',
+    file: null,
+    fileName: null,
+    from: '',
+    to: ''
+  },
+  methods: {
+    changeFramework: function() {
+      this.extension = this.extensions[this.framework]
+    },
+    uploadFile: function(event) {
+      this.file = event.target.files[0]
+      this.fileName = "Selected: " + event.target.files[0].name
+    }
   }
 })
