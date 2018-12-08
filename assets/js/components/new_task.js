2408855d5a7e33f01bdc99cebf39bf6ebe887964
kortirso <kortirso@gmail.com>
2018-12-08 14:13:31 +0200

    update new_task component

--- assets/js/components/new_task.js
+++ assets/js/components/new_task.js
@@ -1,66 +1,79 @@
 import VueResource from 'vue-resource'
 
+const extensions = {'ruby_on_rails': '.yml'}
+const locales = {'en': 'English', 'ru': 'Russian', 'da': 'Danish'}
+
 Vue.use(VueResource)
 
-new Vue({
-  el: '#new_task',
-  data: {
-    extensions: {'ruby_on_rails': '.yml'},
-    locales: {'en': 'English', 'ru': 'Russian', 'da': 'Danish'},
-    framework: '',
-    file: null,
-    fileName: null,
-    from: {code: '', name: ''},
-    to: ''
-  },
-  computed: {
-    extension: function() {
-      return this.framework === '' ? '' : this.extensions[this.framework]
+if ($('#new_task').length) {
+  new Vue({
+    el: '#new_task',
+    data: {
+      framework: '',
+      file: null,
+      fileName: null,
+      from: '',
+      to: '',
+      error: null
     },
-    original_language: function() {
-      return this.from.name === '' ? 'Autodetection' : this.from.name
-    }
-  },
-  watch: {
-    framework: function() {
-      this.file = null
-      this.fileName = null
-      this.from = {code: '', name: ''}
+    computed: {
+      extension: function() {
+        return this.framework === '' ? '' : extensions[this.framework]
+      },
+      originalLanguage: function() {
+        return this.from === '' ? 'Autodetection' : locales[this.from]
+      },
+      fileError: function() {
+        return this.error !== null
+      }
     },
-    file: function() {
-      this.to = ''
-    }
-  },
-  methods: {
-    uploadFile: function(event) {
-      // update file
-      this.file = event.target.files[0]
-      this.fileName = "Selected: " + event.target.files[0].name
-      // send file for locale autodetection
-      let data = new FormData()
-      data.append('file', this.file)
-      data.append('_csrf_token', $('#_csrf_token').val())
-      const config = { header : { 'Content-Type' : 'multipart/form-data' } }
-      this.$http.post('http://localhost:4000/tasks/detection', data, config).then(function(data) {
-        if (data.body.code !== undefined) {
-          const locale = this.locales[data.body.code]
-          if (locale !== undefined) this.from = {code: data.body.code, name: locale}
-          else this.from = {code: '', name: 'Locale is not supported'}
-        } else this.from = {code: '', name: data.body.error}
-      })
+    watch: {
+      framework: function() {
+        this.file = null
+        this.fileName = null
+        this.setError(null)
+      },
+      file: function() {
+        this.to = ''
+      }
     },
-    createTask: function() {
-      let data = new FormData()
-      data.append('task[file]', this.file)
-      data.append('task[from]', this.from.code)
-      data.append('task[to]', this.to)
-      data.append('task[user_session_id]', $('#user_session_id').val())
-      data.append('task[status]', 'created')
-      data.append('_csrf_token', $('#_csrf_token').val())
-      const config = { header : { 'Content-Type' : 'multipart/form-data' } }
-      this.$http.post('http://localhost:4000/tasks', data, config).then(function(data) {
-        this.framework = ''
-      })
+    methods: {
+      uploadFile: function(event) {
+        // update file
+        this.file = event.target.files[0]
+        this.fileName = "Selected: " + event.target.files[0].name
+        // send file for locale autodetection
+        let data = new FormData()
+        data.append('file', this.file)
+        data.append('_csrf_token', $('#_csrf_token').val())
+        const config = { header : { 'Content-Type' : 'multipart/form-data' } }
+        this.$http.post('http://localhost:4000/tasks/detection', data, config).then(function(data) {
+          if (data.body.code !== undefined) {
+            const locale = locales[data.body.code]
+            if (locale !== undefined) {
+              this.from = data.body.code
+              this.error = null
+            } else this.setError('Locale is not supported')
+          } else this.setError(data.body.error)
+        })
+      },
+      setError: function(error) {
+        this.from = ''
+        this.error = error
+      },
+      createTask: function() {
+        let data = new FormData()
+        data.append('task[file]', this.file)
+        data.append('task[from]', this.from)
+        data.append('task[to]', this.to)
+        data.append('task[user_session_id]', $('#user_session_id').val())
+        data.append('task[status]', 'created')
+        data.append('_csrf_token', $('#_csrf_token').val())
+        const config = { header : { 'Content-Type' : 'multipart/form-data' } }
+        this.$http.post('http://localhost:4000/tasks', data, config).then(function(data) {
+          this.framework = ''
+        })
+      }
     }
-  }
-})
+  })
+}
