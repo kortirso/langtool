5edfea30105138177bf328fb9c99eb8f713680aa
kortirso <kortirso@gmail.com>
2019-01-07 22:21:13 +0300

    render users list

--- lib/langtool_web/controllers/users_controller.ex
+++ lib/langtool_web/controllers/users_controller.ex
@@ -1,11 +1,13 @@
 defmodule LangtoolWeb.UsersController do
   use LangtoolWeb, :controller
+  alias Langtool.{Accounts}
 
   plug :check_auth when action in [:index]
 
   def index(conn, _) do
     conn
     |> authorize(:user, :index?, nil)
+    |> assign(:users, Accounts.list_users())
     |> render("index.html")
   end
 end
